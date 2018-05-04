eebc8ebdf0af9d2a019fc80e722f4e94d1f0b2f5
jinxiaonb <315232021@163.com>
2018-05-04 18:35:23 +0800

    add some picture

--- src/h5_static/js/cbkt.js
+++ src/h5_static/js/cbkt.js
@@ -19,23 +19,23 @@ new Vue({
 				name:"课程特色",
 				desc:"吸收蒙特梭利、华德福教育方式的优势，并融合情感教育和传统教育的精髓，打造具备初本幼园特色的课程体系，从八大维度进行幼儿全方位启蒙。",
 				list:[{
-					img:'/img/cbkt/klcf.jpeg',
+					img:'/img/cbkt/klcf.png',
 					name:"快乐厨房",
-					img1:'/img/cbkt/ydly.jpeg',
+					img1:'/img/cbkt/ydly.png',
 					name1:"运动乐园"
 				},{
-					img:'/img/cbkt/tsgjx.jpeg',
+					img:'/img/cbkt/tsgjx.png',
 					name:"图书馆教学",
-					img1:'/img/cbkt/ylsg.jpeg',
+					img1:'/img/cbkt/ylsg.png',
 					name1:"韵律时光"
 				},{
-					img:'/img/cbkt/yscy.jpeg',
+					img:'/img/cbkt/yscy.png',
 					name:"艺术创游",
-					img1:'/img/cbkt/lyxg.jpeg',
+					img1:'/img/cbkt/lyxg.png',
 					name1:"礼仪习惯"
 				}],
 				zlqm:{
-					img:'/img/cbkt/zlqm.jpeg',
+					img:'/img/cbkt/zlqm.png',
 					name:"智力启蒙"
 				}
 			},
